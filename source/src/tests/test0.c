2bf717fbaeda2ba5e73e8012f070a4bb17697be9
Alexander Bakst <abakst@galois.com>
2020-05-20 15:41:03 -0700

    Fix test0 input

--- source/src/tests/test0.c
+++ source/src/tests/test0.c
@@ -7,7 +7,7 @@ main(int argc, char **argv)
 {
     bvrs_ctxt_t *ctxt;
 
-    status_t openok = open_db("testdb.db", &ctxt);
+    status_t openok = open_db(argv[1], &ctxt);
     assert(openok == OK);
 
     status_t closeok = close_db(ctxt);
