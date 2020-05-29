5b55bd7211747b03b679d618f2f7ef8367325f7a
Lyell Read <lyellread@gmail.com>
2020-05-28 21:20:55 -0700

    Added html generator for voter data

--- public/registration_verification.js
+++ public/registration_verification.js
@@ -32,3 +32,27 @@ document.addEventListener('submit', e => {
   e.preventDefault();
 
 });
+
+
+function fillVoterData (/*data*/){
+
+	var data = '{"voter_q":[{"id":101,"lastname":"lastname","givennames":"firstname","resaddress":"1234 address","mailaddress":"1234 address","registeredparty":"whig","birthdate":507859200,"idinfo":"YmxvYjE=","status":0,"initialregtime":0,"lastupdatetime":0,"confidential":0}]}';
+
+	jsonData = JSON.parse(data);
+
+	var div = document.getElementById('voterDataInner');
+
+	div.innerHTML += "<div class=voterDataItem>Given Name(s): " + jsonData['voter_q'][0]["givennames"] + "</div>";
+	div.innerHTML += "<div class=voterDataItem>Last Name: " + jsonData['voter_q'][0]["lastname"] + "</div>";
+	div.innerHTML += "<div class=voterDataItem>Residential Address: " + jsonData['voter_q'][0]["resaddress"] + "</div>";
+	div.innerHTML += "<div class=voterDataItem>Mailing Address: " + jsonData['voter_q'][0]["mailaddress"] + "</div>";
+	div.innerHTML += "<div class=voterDataItem>Registered Party: " + jsonData['voter_q'][0]["registeredparty"] + "</div>";
+	div.innerHTML += "<div class=voterDataItem>Birthdate: " + jsonData['voter_q'][0]["birthdate"] + "</div>";
+	
+	if (jsonData['voter_q'][0]["status"] === '0'){
+		div.innerHTML += "<div class=voterDataItem>Registration Status: Inactive" + "</div>";
+	}
+	else{
+		div.innerHTML += "<div class=voterDataItem>Registration Status: Active" + "</div>";
+	}
+}
