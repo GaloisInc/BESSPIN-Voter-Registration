78a66edb1963b8f839e590bf86c0ee97625877ef
Lyell Read <lyellread@gmail.com>
2020-06-03 16:47:45 -0700

    Adding date formats, and populating dropdowns for election_official_query.html

--- public/query.js
+++ public/query.js
@@ -6,12 +6,15 @@ function emptyTable(){
 	div.innerHTML = `<table cellspacing="0" cellpadding="5" id=queryTable>
             <tr>
             	<th> Select </th>
+            	<th> Voter ID </th>
                 <th> First Name </th>
                 <th> Last Name </th>
                 <th> Residential Address </th>
                 <th> Mailing Address </th>
                 <th> Registered Party </th>
                 <th> Birthdate </th>
+                <th> Initial Reg. Time </th>
+                <th> Last Reg. Update </th>
                 <th> Status </th>
             </tr>
         </table>`;
@@ -38,13 +41,31 @@ function addVoterToTable(/*voter*/){
 
 	div.innerHTML += "<tr>" + 
 					'<td><input type="checkbox" id="' + ID + '" name="' + ID + '"></td>' +
+					"<td>" + ID + "</td>" +
 					"<td>"+ jsonData['voter_q'][0]["givennames"] + "</td>" +
 					"<td>" + jsonData['voter_q'][0]["lastname"] + "</td>" +
 					"<td>" + jsonData['voter_q'][0]["resaddress"] + "</td>" +
 					"<td>" + jsonData['voter_q'][0]["mailaddress"] + "</td>" +
 					"<td>" + jsonData['voter_q'][0]["registeredparty"] + "</td>" +
-					"<td>" + jsonData['voter_q'][0]["birthdate"] + "</td>" +
-					status + 
+					"<td>" + formatDate(jsonData['voter_q'][0]["birthdate"]) + "</td>" +
+					"<td>" + formatDate(jsonData['voter_q'][0]["initialregtime"]) + "</td>" + 
+					"<td>" + formatDate(jsonData['voter_q'][0]["lastupdatetime"]) + "</td>" + 
+					status +
 					"</tr>";
 
+}
+
+function formatDate(epoch_time) {
+    var d = new Date(0);
+    d.setUTCSeconds(epoch_time);
+    month = '' + (d.getMonth() + 1);
+    day = '' + d.getDate();
+    year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
 }
